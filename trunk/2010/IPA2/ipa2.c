860bc7eabc2fa1a6484a440f3c03fe4b4a6f0a6f
askia.hill <askia.hill@1b976a62-fd5c-11de-8589-c9554b7fe0d5>
2010-01-27 17:54:51 +0000

    IPA2 skeleton uploaded

---
+++ trunk/2010/IPA2/ipa2.c
@@ -0,0 +1,7 @@
+/*
+ * Main function should control the flow of your program a
+ */
+int main()
+{
+  return 0;
+}
---
+++ trunk/2010/IPA2/student.c
@@ -0,0 +1,90 @@
+/*
+ * Name: AddStudent
+ *
+ * Inputs: The database and the input file
+ * Returns: N/A
+ * 
+ * Reads in the students data from the file and inserts it into the database
+ */
+void AddStudent()
+{
+}
+
+/*
+ * Name: Search
+ * Inputs: The database and the input file
+ * Returns: N/A
+ * 
+ * Determines whether an ID number or name is being read and calls the
+ * appropriate Search function
+ */
+void Search()
+{
+}
+
+/*
+ * Name : SearchID
+ * Inputs: The database and the ID number to find
+ * Returns: N/A
+ *
+ * Finds the database entry with the same ID number 
+ */
+void SearchID()
+{
+}
+
+/*
+ * Name : SearchName
+ * Inputs: The database and the name to find
+ * Returns: N/A
+ *
+ * Finds the database entries with the same name
+ */
+void SearchName()
+{
+}
+
+/*
+ * Name : Delete
+ * Inputs: The database and the input file
+ * Returns: N/A
+ *
+ * Determines what is to be used as the search key and calls the appropriate
+ * Delete function
+ */
+void Delete()
+{
+}
+
+/*
+ * Name : DeleteID
+ * Inputs: The database and ID number to find
+ * Returns: N/A
+ *
+ * Finds the database entry with the ID number and removes it from the database
+ */
+void DeleteID()
+{
+}
+
+/*
+ * Name : DeleteName
+ * Inputs: The database and the name to find
+ * Returns: N/A
+ *
+ * Finds the database entries with the names and removes them from the database
+ */
+void DeleteName()
+{
+}
+
+/*
+ * Name : PrintEntry
+ * Inputs: The database entry to be printed and control value
+ * Returns: N/A
+ *
+ * Prints the appropriate data user is searching for based on control value
+ */
+void PrintEntry()
+{
+}
