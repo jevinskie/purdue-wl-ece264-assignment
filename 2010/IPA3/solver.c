58e3ab051f14b54153e3af92c121727570d77bf5
rnpa.pigpag <rnpa.pigpag@gmail.com>
2010-02-10 23:11:26 +0000

    IPA3 and IPA4 codes added

---
+++ 2010/IPA3/solver.c
@@ -0,0 +1,33 @@
+/*
+  solver.c
+  This module provides algorithms to solve a maze.
+  It has nothing to do with file operation/maze construction.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "solver.h"
+#include "maze.h"
+#include "linklist.h"
+#include "stack.h"
+
+/* TODO Add additional helper functions here, BEFORE SolveMaze 
+ * function */
+
+int SolveMaze(Maze * maze)
+{
+  /* Main function to solve the maze problem.  However, you may
+   * want to create additional functions to help with the task.
+   */
+
+  /* TODO Initialize dynamic data structures here */
+
+  /* TODO Solve the problem using back track */
+
+  /* TODO Destruct dynamic data structures here */
+
+  return 0;  /* Maze solved successfully */
+}
+
