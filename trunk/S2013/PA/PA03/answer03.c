7deb8d8b14b0b4b6a1fc7729798a57baa22c8419
luyunghsiang@gmail.com <luyunghsiang@gmail.com@1b976a62-fd5c-11de-8589-c9554b7fe0d5>
2013-01-02 02:35:24 +0000

    assignment 3 recursion

---
+++ trunk/S2013/PA/PA03/answer03.c
@@ -0,0 +1,30 @@
+/*
+ * Please fill the functions in this file.
+ * You can add additional functions. 
+ *
+ * Hint: It is very likely that you need to write additonal functions.
+ */
+
+#include "pa03.h"
+#include <stdio.h>
+#include <stdlib.h>
+void partition(int value)
+{
+  printf("partition %d\n", value);
+}
+/*
+ * =================================================================
+ */
+
+void subset(char * charset, int length)
+{
+  printf("subset of ");
+}
+/*
+ * =================================================================
+ */
+void permute(char * charset, int length)
+{
+  printf("permute ");
+}
+
---
+++ trunk/S2013/PA/PA03/pa03.h
@@ -0,0 +1,9 @@
+/*
+ * Do not modify this file
+ */
+#ifndef PA03_H
+#define PA03_H
+void partition(int value);
+void subset(char * charset, int length);
+void permute(char * charset, int length);
+#endif
