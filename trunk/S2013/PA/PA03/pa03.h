daae0e9b85b044a4758069db9d4fed72d8a9f818
luyunghsiang@gmail.com <luyunghsiang@gmail.com@1b976a62-fd5c-11de-8589-c9554b7fe0d5>
2013-01-11 20:06:42 +0000

    move the constants for the header

--- trunk/S2013/PA/PA03/pa03.h
+++ trunk/S2013/PA/PA03/pa03.h
@@ -3,6 +3,8 @@
  */
 #ifndef PA03_H
 #define PA03_H
+#define MINLENGTH 3
+#define MAXLENGTH 8
 void partition(int value);
 void subset(char * charset, int length);
 void permute(char * charset, int length);
